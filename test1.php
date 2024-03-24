13bb4128c943002734ad9a6e2d563b468cf7142e
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-03-24 14:38:37 +0200

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,16 +1,23 @@
 <?php
 //Вдалі
 
-function addTwoNumbers($num1, $num2) {
-    return $num1 + $num2;
+function divisionTwoNumbers($num1, $num2) {
+    return $num1 / $num2;
 }
 function concatenateStrings($str1, $str2) {
     return $str1 . $str2;
 }
 //Невдалі
-function rectangleArea($width, $height) {
-    return $width * $height;
-}
+function factorial($num) {
+    if ($num < 0) {
+        return "Can't calculate factorial of a negative number";
+    } else {
+        $factorial = 1;
+        for ($i = 1; $i <= $num; $i++) {
+            $factorial *= $i;
+        }
+        return $factorial;
+    }
 function circleArea($radius) {
     return $radius * $radius * 3; // The result should be multiplied by PI, not 3
 }
