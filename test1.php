9349bc48311ea3363636504ea027b42ca04d88ae
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-03-24 14:15:10 +0200

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,10 +1,17 @@
 <?php
 //Вдалі
 
+function addTwoNumbers($num1, $num2) {
+    return $num1 + $num2;
+}
+function concatenateStrings($str1, $str2) {
+    return $str1 . $str2;
+}
 //Невдалі
-// Функція, яка генерує помилку (невдала)
-function generateError() {
-    // Спроба доступу до неіснуючої змінної
-    return $undefinedVariable;
+function rectangleArea($width, $height) {
+    return $width * $height;
+}
+function circleArea($radius) {
+    return $radius * $radius * 3; // The result should be multiplied by PI, not 3
 }
 ?>
