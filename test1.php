ff0b267a937bcfc0471553f1d386b94b88b155b7
Maksim <93036602+KREIK2017@users.noreply.github.com>
2024-03-24 14:10:29 +0200

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,22 +1,10 @@
 <?php
-// невдалі 
-// Функція для вилучення елементу з масиву за індексом
-function removeElement($array, $index) {
-    if (array_key_exists($index, $array)) {
-        unset($array[$index]);
-        return $array;
-    } else {
-        return "Index doesn't exist in the array";
-    }
-}
+//Вдалі
 
-// Функція для ділення двох чисел
-function divide($a, $b) {
-    if ($b != 0) {
-        return $a / $b;
-    } else {
-        return "Can't divide by zero";
-    }
+//Невдалі
+// Функція, яка генерує помилку (невдала)
+function generateError() {
+    // Спроба доступу до неіснуючої змінної
+    return $undefinedVariable;
 }
-
 ?>
